38b181f447670ff636c78413b3741699a0f4d9be
Martin Milev <martinmariusmilev@gmail.com>
2023-03-09 12:18:03 +0200

    Add UI library
    Added Ant Designe ui library.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import './App.css'
 import React from 'react';
+import { CloseOutlined, SearchOutlined } from '@ant-design/icons';
+import { Input, Tooltip } from 'antd';
+import { Typography } from 'antd';
+
+const { Title } = Typography;
 
 const App = () => (
   <div>
-    Search X
+    <Title>Search X</Title>
+    <Input
+      placeholder='Search'
+      prefix={<SearchOutlined className='site-form-item-icon' />}
+      suffix={
+        <Tooltip title='Clear'>
+          <CloseOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+        </Tooltip>
+      }
+    />
   </div>
 )
 
